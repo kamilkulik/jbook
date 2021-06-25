096d6311685a95f6bc3b4dcca919d56deeafcef5
kamilkulikTeck <kulikkamil@gmail.com>
2021-06-25 08:33:00 +0200

    calculate path entered by user and pass it off to local-api

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,10 +1,13 @@
 import { Command } from 'commander';
 import { serve } from 'local-api';
+import path from 'path';
 
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server on', '4005')
   .action((filename = 'notebook.js', options: { port: string }) => {
-    serve(parseInt(options.port), filename, '/');
+    const dir = path.join(process.cwd(), path.dirname(filename));
+
+    serve(parseInt(options.port), path.basename(filename), dir);
   });
