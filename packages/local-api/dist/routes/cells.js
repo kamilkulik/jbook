d0a616bb8a9f4d5fc96a8ec3505a7221e36030f8
kamilkulikTeck <kulikkamil@gmail.com>
2021-07-02 08:08:58 +0200

    add path, fs modules and logic to the router

--- packages/local-api/dist/routes/cells.js
+++ packages/local-api/dist/routes/cells.js
@@ -41,16 +41,49 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createCellsRouter = void 0;
 var express_1 = __importDefault(require("express"));
+var promises_1 = __importDefault(require("fs/promises"));
+var path_1 = __importDefault(require("path"));
 var createCellsRouter = function (filename, dir) {
     var router = express_1.default.Router();
+    var fullPath = path_1.default.join(dir, filename);
     router.get('/cells', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+        var result, err_1;
         return __generator(this, function (_a) {
-            return [2 /*return*/];
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 2, , 3]);
+                    return [4 /*yield*/, promises_1.default.readFile(fullPath, { encoding: 'utf-8' })];
+                case 1:
+                    result = _a.sent();
+                    res.send(JSON.parse(result));
+                    return [3 /*break*/, 3];
+                case 2:
+                    err_1 = _a.sent();
+                    if (err_1.code === 'ENOENT') {
+                        // Add code to create a file and add default cells
+                    }
+                    else {
+                        throw new Error(err_1);
+                    }
+                    return [3 /*break*/, 3];
+                case 3: return [2 /*return*/];
+            }
         });
     }); });
     router.post('/cells', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+        var cells;
         return __generator(this, function (_a) {
-            return [2 /*return*/];
+            switch (_a.label) {
+                case 0:
+                    cells = req.body.cells;
+                    // serialise them, Write the cells into the file
+                    return [4 /*yield*/, promises_1.default.writeFile(fullPath, JSON.stringify(cells), 'utf-8')];
+                case 1:
+                    // serialise them, Write the cells into the file
+                    _a.sent();
+                    res.send({ status: 'ok' });
+                    return [2 /*return*/];
+            }
         });
     }); });
     return router;
--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -1,22 +1,44 @@
 import express from 'express';
+import fs from 'fs/promises';
+import path from 'path';
+
+interface Cell {
+  id: string;
+  content: string;
+  type: 'text' | 'code';
+}
 
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
 
+  const fullPath = path.join(dir, filename);
+
   router.get('/cells', async (req, res) => {
-    // Make sure the cell storage file exists
-    // If it does not exist add in a default list of cells
-    // Read the file
+    try {
+      // Read the file
+      const result = await fs.readFile(fullPath, { encoding: 'utf-8' });
+
+      res.send(JSON.parse(result));
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        // Add code to create a file and add default cells
+      } else {
+        throw new Error(err);
+      }
+    }
+    // If read throws an error
+    // Inspect the error, see if it ays that the file doesn't exist
     // Parse a list of cells out of it
     // send list of lists cells back to the browser
   });
 
   router.post('/cells', async (req, res) => {
-    // Make sure the file exists
-    // If not, create it
     // Take the list of cells from the request obj
-    // serialise them
-    // Write the cells into the file
+    const { cells }: { cells: Cell[] } = req.body;
+    // serialise them, Write the cells into the file
+    await fs.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
+
+    res.send({ status: 'ok' });
   });
 
   return router;
