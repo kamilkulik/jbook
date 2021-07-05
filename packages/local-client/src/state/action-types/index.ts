af3687291bc0cf6f567bdd5827addb497beea0f6
kamilkulikTeck <kulikkamil@gmail.com>
2021-07-05 08:31:29 +0200

    add save cells action type

--- packages/local-client/src/state/action-types/index.ts
+++ packages/local-client/src/state/action-types/index.ts
@@ -1,11 +1,12 @@
 export enum ActionType {
+  BUNDLE_START = 'bundle_start',
+  BUNDLE_COMPLETE = 'bundle_complete',
   DELETE_CELL = 'delete_cell',
   FETCH_CELLS = 'fetch_cells',
   FETCH_CELLS_COMPLETE = 'fetch_cells_complete',
   FETCH_CELLS_ERROR = 'fetch_cells_error',
   INSERT_CELL_AFTER = 'insert_cell_after',
   MOVE_CELL = 'move_cell',
+  SAVE_CELLS_ERROR = 'save_cells_error',
   UPDATE_CELL = 'update_cell',
-  BUNDLE_START = 'bundle_start',
-  BUNDLE_COMPLETE = 'bundle_complete',
 }
